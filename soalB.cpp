c31652c72ae5a1d5fa60fbefffcbff94bf423a86
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-05-17 16:29:42 +0700

    membuat class dan hak akses untuk friend class yang sudah di tentukan dan beri hak akses untuk petugas

--- soalB.cpp
+++ soalB.cpp
@@ -2,3 +2,27 @@
 #include <string>
 using namespace std;
 
+class Petugas;
+class Admin;
+class peminjam;
+
+class Buku
+{
+    private:
+        string judul;
+        string penulis;    
+        bool dipinjam;
+    public:
+        friend class Admin;
+        friend class peminjam;
+        void prosespinjam(Buku* b, Peminjam* p);   
+        void prosesKembali(Buku* b); 
+};
+
+class Peminjam
+{
+    private:
+        string nama;
+        int ID;
+        
+};
