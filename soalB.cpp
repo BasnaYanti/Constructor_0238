1663b3fbe3e3e39b72ab1d870be664bbfb1ddec5
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-05-17 18:40:32 +0700

    last kommit

--- soalB.cpp
+++ soalB.cpp
@@ -15,8 +15,8 @@ class Buku
     public:
         friend class Admin;
         friend class peminjam;
-        void prosespinjam(Buku* b, Peminjam* p);   
-        void prosesKembali(Buku* b); 
+        friend void Petugas :: prosesPinjam (Buku* b, Peminjam* p);   
+        friend void Petugas :: prosesKembali (Buku* b);   
 };
 
 class Peminjam
@@ -24,5 +24,55 @@ class Peminjam
     private:
         string nama;
         int ID;
-        
+        int totalPinjaman;
+
+    public:
+        friend class Admin;
+        friend class Petugas;
+        friend void lihatTotalPinjaman(Buku* b, Peminjam* p);
 };
+
+class Petugas
+{
+    private:
+    string nama;
+    int ID;
+    string levelAkses;
+
+    public:
+        void prosesPinjam(Buku* b, Peminjam* p);
+        void prosesKembali(Buku* b);
+        friend class Admin;
+};
+
+
+class Admin
+{
+    public:
+    void tampilkanStatistik(Buku* b, Peminjam* p)
+    {
+        cout << "Nama Peminjam: " << p->nama << endl;
+        cout << "ID Peminjam: " << p->ID << endl;
+        cout << "Total Pinjaman: " << p->totalPinjaman << " buku" << endl;
+        cout << "Status Buku: " << (b->dipinjam ? "Sedang Dipinjam" : "Tersedia") << endl;
+    }
+    void lihatTotalPinjaman(Peminjam* p)
+    {
+         cout << "Total Pinjaman oleh " << p->nama << p->totalPinjaman;
+    }
+    void lihatStatusBuku(Buku* b)
+    {
+        cout << "Status buku: " << (b->dipinjam ? "Sedang Dipinjam" : "Tersedia") << endl;
+    }
+};
+
+
+int main()
+{
+    Buku b;
+    Peminjam p;
+    Admin admin;
+    
+
+    return 0;
+}
